e4d8b05378fbc9b8faad59148928a2d48108e53b
immaculateben <kunlegem@gmail.com>
2023-03-30 15:21:01 -0700

    linked

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- * print_list - prints all the elements of a list_t list
- * @h: singly linked list
- * Return: number of elements in the list
+ * print_list - prints all the elements of a list_t list.
+ * @h: singly linked list.
+ * Return: number of elements in the list.
 */
 
 size_t print_list(const list_t *h)
@@ -21,3 +21,4 @@ size_t print_list(const list_t *h)
 	}
 	return (nelem);
 }
+
